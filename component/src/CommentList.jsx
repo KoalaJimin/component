28ed057180db5a29765d500c3699dc2fe011f2cc
Jimin <kgmline2@naver.com>
2023-05-12 01:25:44 +0900

    third

--- component/src/CommentList.jsx
+++ component/src/CommentList.jsx
@@ -2,6 +2,7 @@ function CommentList(props) {
   return (
     <div>
       <Comment name={"김멋사"} comment={"안녕하세요 김멋사입니다"} />
+      <Comment name={"김솜솜"} comment={"안녕하세요 저는 솜솜이에요"} />
     </div>
   );
 }
